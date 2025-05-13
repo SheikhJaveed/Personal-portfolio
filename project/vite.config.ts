0c4192b8d016733e9e2d88d6bfc0a837d578b8eb
Sheikh Javeed <sheikhmannanjaveed@gmail.com>
2025-05-13 18:20:09 +0530

    updated config files

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['lucide-react'], // <-- force pre-bundling
+  },
   build: {
     rollupOptions: {
-      external: [] // make sure lucide-react is not here
+      external: [], // make sure lucide-react is NOT here
     }
   }
 });
